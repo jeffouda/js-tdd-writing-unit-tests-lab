83652cfe65ffce43aa21eb01d34033232b97ec2c
Jeff Ouda <jeffoudahabakuk@gmail.com>
2025-10-24 09:51:14 +0300

    test done

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,11 @@
-// Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+  test('returns true if word is "racecar"', () => {
+    expect(isPalindrome("racecar")).toBe(true);
+  });
+
+  test('returns false if word is "car"', () => {
+    expect(isPalindrome("car")).toBe(false);
+  });
+});
